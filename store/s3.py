9d9867a98b15054982fb797e9c93c62d278ac406
Eun-Gyu Kim <eungyu.kim@gmail.com>
2012-07-12 23:37:14 -0700

    removed keys

--- store/s3.py
+++ store/s3.py
@@ -1,17 +1,16 @@
+from store import app
+
 from time import time
 import base64
 import hmac, sha
 import urllib
 
-AWSAccessKeyId = 'AKIAIVD52EDQ5XLSF7GA'
-AWSSecretAccessKey = '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'
-
 def url(bucket, key, secondsAlive):
 	expires = int(time()) + secondsAlive
 	canonicalizedResource = urllib.quote("/" + bucket + "/" + key)
 	stringToSign = "GET\n\n\n" + str(expires) + "\n" + canonicalizedResource
-	signature = base64.b64encode(hmac.new(AWSSecretAccessKey, stringToSign, sha).digest())
+	signature = base64.b64encode(hmac.new(app.config['AWS_SECRET_ACCESS_KEY'], stringToSign, sha).digest())
 	return "http://"+bucket+".s3.amazonaws.com/"+urllib.quote(key)+ \
-    "?AWSAccessKeyId="+urllib.quote(AWSAccessKeyId)+ \
+    "?AWSAccessKeyId="+urllib.quote(app.config['AWS_ACCESS_KEY_ID'])+ \
     "&Expires="+str(expires)+"&Signature="+urllib.quote(signature)
 
